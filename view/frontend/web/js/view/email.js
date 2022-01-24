f21aef825516076dbee38cc38856d2c31ef413ea
Mark Shust <mark@shust.com>
2022-01-24 15:10:13 -0500

    Add a new step to the checkout header

---
+++ view/frontend/web/js/view/email.js
@@ -0,0 +1,39 @@
+define([
+    'uiComponent',
+    'ko',
+    'Magento_Checkout/js/model/step-navigator',
+    'mage/translate',
+    'underscore'
+], function(
+    Component,
+    ko,
+    stepNavigator,
+    $t,
+    _
+) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            template: 'Macademy_CustomCheckout/email',
+            isVisible: ko.observable(false),
+        },
+        initialize: function() {
+            this._super();
+
+            stepNavigator.registerStep(
+                'email',
+                null,
+                $t('Email'),
+                this.isVisible,
+                _.bind(this.navigate, this),
+                this.sortOrder
+            );
+
+            return this;
+        },
+        navigate: function() {
+            this.isVisible(true);
+        }
+    });
+});

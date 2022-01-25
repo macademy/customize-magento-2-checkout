357e0bcf4ae80624286c47ed28b3585106cbb4a6
Mark Shust <mark@shust.com>
2022-01-25 10:52:41 -0500

    Alternative method of overriding Knockout.js templates

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,5 +1,11 @@
 let config = {
     deps: [
         'Macademy_CustomCheckout/js/mask-telephone-inputs'
-    ]
+    ],
+    map: {
+        '*': {
+            'Magento_Checkout/template/sidebar.html':
+                'Macademy_CustomCheckout/template/sidebar.html'
+        }
+    }
 };

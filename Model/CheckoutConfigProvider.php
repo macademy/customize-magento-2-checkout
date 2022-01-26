9972d9059ea97b0e010812bc557ebb71b7dd645b
Mark Shust <mark@shust.com>
2022-01-26 12:11:23 -0500

    Add a CMS block to the sidebar with a checkout config provider

--- Model/CheckoutConfigProvider.php
+++ Model/CheckoutConfigProvider.php
@@ -3,13 +3,24 @@
 namespace Macademy\CustomCheckout\Model;
 
 use Magento\Checkout\Model\ConfigProviderInterface;
+use Magento\Framework\View\LayoutInterface;
 
 class CheckoutConfigProvider implements ConfigProviderInterface
 {
+    private $fulfillmentStatus;
+
+    public function __construct(
+        LayoutInterface $layout
+    ) {
+        $this->fulfillmentStatus = $layout->createBlock('Magento\Cms\Block\Block')
+            ->setBlockId('fulfillment_status')
+            ->toHtml();
+    }
+
     public function getConfig(): array
     {
         return [
-            'myKey' => 'myValue',
+            'fulfillment_status' => $this->fulfillmentStatus,
         ];
     }
 }

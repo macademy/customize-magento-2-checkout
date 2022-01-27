3346ebc166eaad2328f6931c0166996d4a3a4153
Mark Shust <mark@shust.com>
2022-01-27 15:01:31 -0500

    Hide a custom attributes from the billing address form

--- Block/Checkout/LayoutProcessor/AddressClassificationAttribute.php
+++ Block/Checkout/LayoutProcessor/AddressClassificationAttribute.php
@@ -19,6 +19,17 @@ class AddressClassificationAttribute implements LayoutProcessorInterface
         $attributeData['config']['customScope'] = 'shippingAddress.custom_attributes';
         $attributeData['dataScope'] = "shippingAddress.custom_attributes.$attributeCode";
 
+        foreach ($jsLayout['components']['checkout']['children']
+                 ['steps']['children']
+                 ['billing-step']['children']
+                 ['payment']['children']
+                 ['payments-list']['children'] as &$paymentMethod) {
+            $fields = &$paymentMethod['children']['form-fields']['children'];
+            if (isset($fields[$attributeCode])) {
+                unset($fields[$attributeCode]);
+            }
+        }
+
         return $jsLayout;
     }
 }

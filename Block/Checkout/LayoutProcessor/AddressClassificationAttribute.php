70635ff62dc6893cca15ad073133204f7ed3e2c8
Mark Shust <mark@shust.com>
2022-01-27 12:41:31 -0500

    Scope the shipping address attribute as a custom attribute

---
+++ Block/Checkout/LayoutProcessor/AddressClassificationAttribute.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Macademy\CustomCheckout\Block\Checkout\LayoutProcessor;
+
+use Magento\Checkout\Block\Checkout\LayoutProcessorInterface;
+
+class AddressClassificationAttribute implements LayoutProcessorInterface
+{
+    public function process($jsLayout): array
+    {
+        $attributeCode = 'address_classification';
+        $attributeData = &$jsLayout['components']['checkout']['children']
+            ['steps']['children']
+            ['shipping-step']['children']
+            ['shippingAddress']['children']
+            ['shipping-address-fieldset']['children']
+            [$attributeCode];
+
+        $attributeData['config']['customScope'] = 'shippingAddress.custom_attributes';
+        $attributeData['dataScope'] = "shippingAddress.custom_attributes.$attributeCode";
+
+        return $jsLayout;
+    }
+}
